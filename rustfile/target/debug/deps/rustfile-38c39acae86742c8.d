30ce57d9cb32953004b26b46945da650b32358d5
uzak0209 <116805788+uzak0209@users.noreply.github.com>
2025-05-31 00:27:32 +0900

    exeファイルの軽微な修正

---
+++ rustfile/target/debug/deps/rustfile-38c39acae86742c8.d
@@ -0,0 +1,5 @@
+C:\Users\kazuk\Documents\GitHub\Automatic-stamping-King-of-Time-\rustfile\target\debug\deps\librustfile-38c39acae86742c8.rmeta: src\main.rs
+
+C:\Users\kazuk\Documents\GitHub\Automatic-stamping-King-of-Time-\rustfile\target\debug\deps\rustfile-38c39acae86742c8.d: src\main.rs
+
+src\main.rs:
---
+++ rustfile/target/debug/deps/rustfile-82266b33af211e17.d
@@ -0,0 +1,5 @@
+C:\Users\kazuk\Documents\GitHub\Automatic-stamping-King-of-Time-\rustfile\target\debug\deps\librustfile-82266b33af211e17.rmeta: src\main.rs
+
+C:\Users\kazuk\Documents\GitHub\Automatic-stamping-King-of-Time-\rustfile\target\debug\deps\rustfile-82266b33af211e17.d: src\main.rs
+
+src\main.rs:
